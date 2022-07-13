f624efcb882b0a17a5c4e31404a8adcd3231ff85
zhangfake <zhangfake2001@outlook.com>
2022-07-13 15:33:19 +0800

    init
    first commit

---
+++ assets/content-script-loader.index.ts.0ab46b91.a6118ac5.js
@@ -0,0 +1,11 @@
+(function () {
+  'use strict';
+
+  (async () => {
+    await import(
+      /* @vite-ignore */
+      chrome.runtime.getURL("assets/index.ts.0ab46b91.js")
+    );
+  })().catch(console.error);
+
+})();
---
+++ service-worker-loader.js
@@ -0,0 +1,1 @@
+import './assets/index.ts.868b7abf.js';
